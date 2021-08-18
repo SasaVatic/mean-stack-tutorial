afe93165e71d149cd89a3395041bca0bcf747c93
SasaVatic <zeklbike@gmail.com>
2021-08-18 22:01:22 +0200

    -added click event for post display

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
+  enteredValue: string = '';
+  newPost: string = '';
 
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
 }
